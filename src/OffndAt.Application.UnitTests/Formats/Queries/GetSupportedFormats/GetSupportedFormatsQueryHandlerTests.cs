9b4b0caca7eadc073057c408f07868e836507a85
Grzegorz Hawliczek <ghawliczek@outlook.com>
2024-06-17 21:51:13 +0200

    chore: add unit tests for get supported formats query handler

---
+++ src/OffndAt.Application.UnitTests/Formats/Queries/GetSupportedFormats/GetSupportedFormatsQueryHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿namespace OffndAt.Application.UnitTests.Formats.Queries.GetSupportedFormats;
+
+using Application.Formats.Queries.GetSupportedFormats;
+using Domain.Enumerations;
+
+internal sealed class GetSupportedFormatsQueryHandlerTests
+{
+    private GetSupportedFormatsQueryHandler _handler = null!;
+
+    [SetUp]
+    public void Setup() => _handler = new GetSupportedFormatsQueryHandler();
+
+    [Test]
+    public async Task Handle_ShouldReturnFormatDtoForEachEntryInFormatEnumeration()
+    {
+        var expected = Format.List.ToList();
+
+        var actual = await _handler.Handle(new GetSupportedFormatsQuery(), CancellationToken.None);
+
+        Assert.Multiple(
+            () =>
+            {
+                Assert.That(actual.HasValue, Is.True);
+                Assert.That(expected, Has.Count.EqualTo(actual.Value.Formats.Count()));
+            });
+
+        foreach (var format in expected)
+        {
+            Assert.That(actual.Value.Formats.Any(dto => dto.Value == format.Value), Is.True);
+        }
+    }
+}
--- src/OffndAt.Application/Formats/Queries/GetSupportedFormats/GetSupportedFormatsQueryHandler.cs
+++ src/OffndAt.Application/Formats/Queries/GetSupportedFormats/GetSupportedFormatsQueryHandler.cs
@@ -5,8 +5,12 @@ using Core.Abstractions.Messaging;
 using Domain.Core.Primitives;
 using Domain.Enumerations;
 
+/// <summary>
+///     Represents the <see cref="GetSupportedFormatsQuery" /> handler.
+/// </summary>
 internal sealed class GetSupportedFormatsQueryHandler : IQueryHandler<GetSupportedFormatsQuery, GetSupportedFormatsResponse>
 {
+    /// <inheritdoc />
     public Task<Maybe<GetSupportedFormatsResponse>> Handle(GetSupportedFormatsQuery request, CancellationToken cancellationToken)
     {
         var formatDtos = Format.List.Select(format => new FormatDto(format.Value, format.Name));
