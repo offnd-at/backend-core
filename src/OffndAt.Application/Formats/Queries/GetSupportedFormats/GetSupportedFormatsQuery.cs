feb4427585c770c548ff4f0802457fd15c220ab4
Grzegorz Hawliczek <ghawliczek@outlook.com>
2024-06-17 21:37:40 +0200

    feat: add supported formats endpoint

---
+++ src/OffndAt.Application/Formats/Queries/GetSupportedFormats/GetSupportedFormatsQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace OffndAt.Application.Formats.Queries.GetSupportedFormats;
+
+using Contracts.Formats;
+using Core.Abstractions.Messaging;
+
+/// <summary>
+///     Represents the query used for getting supported formats.
+/// </summary>
+public sealed class GetSupportedFormatsQuery : IQuery<GetSupportedFormatsResponse>;
---
+++ src/OffndAt.Contracts/Formats/GetSupportedFormatsResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace OffndAt.Contracts.Formats;
+
+/// <summary>
+///     Represents the supported formats response.
+/// </summary>
+/// <param name="formats">The supported formats collection.</param>
+public sealed class GetSupportedFormatsResponse(IEnumerable<FormatDto> formats)
+{
+    /// <summary>
+    ///     Gets the supported formats collection.
+    /// </summary>
+    public IEnumerable<FormatDto> Formats { get; } = formats;
+}
