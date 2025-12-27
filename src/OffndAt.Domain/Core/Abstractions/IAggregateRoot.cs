219ef64f558b598f248690dc196bed9fec48b6aa
Grzegorz Hawliczek <ghawliczek@outlook.com>
2025-12-27 15:24:26 +0100

    feat: update target framework to .NET 10

--- src/OffndAt.Domain/Core/Abstractions/IAggregateRoot.cs
+++ src/OffndAt.Domain/Core/Abstractions/IAggregateRoot.cs
@@ -10,10 +10,10 @@ public interface IAggregateRoot
     /// <summary>
     ///     Gets the domain events. This collection is readonly.
     /// </summary>
-    public IReadOnlyCollection<IDomainEvent> DomainEvents { get; }
+    IReadOnlyCollection<IDomainEvent> DomainEvents { get; }
 
     /// <summary>
     ///     Clears all the domain events from the <see cref="IAggregateRoot" />.
     /// </summary>
-    public void ClearDomainEvents();
+    void ClearDomainEvents();
 }
