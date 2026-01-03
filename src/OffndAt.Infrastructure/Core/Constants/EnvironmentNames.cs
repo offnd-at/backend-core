5639094bb765bd8910e207b5c4df985f23339ea5
Grzegorz Hawliczek <ghawliczek@outlook.com>
2026-01-03 22:58:39 +0100

    chore: minor fixes

--- src/OffndAt.Infrastructure/Core/Constants/EnvironmentNames.cs
+++ src/OffndAt.Infrastructure/Core/Constants/EnvironmentNames.cs
@@ -7,8 +7,23 @@ namespace OffndAt.Infrastructure.Core.Constants;
 /// </summary>
 public static class EnvironmentNames
 {
+    /// <summary>
+    ///     Represents the environment name for the development stage of the application lifecycle.
+    /// </summary>
     public static readonly string Development = Environments.Development;
+
+    /// <summary>
+    ///     Represents the environment name for the staging stage of the application lifecycle.
+    /// </summary>
     public static readonly string Staging = Environments.Staging;
+
+    /// <summary>
+    ///     Represents the environment name for the production stage of the application lifecycle.
+    /// </summary>
     public static readonly string Production = Environments.Production;
+
+    /// <summary>
+    ///     Specifies the environment name for the testing stage of the application lifecycle.
+    /// </summary>
     public static readonly string Testing = nameof(Testing);
 }
